538e3af6bdd34e1e5d5b4ba29a3824fa54a9aaba
bonk-dev <github.p0j6t@passinbox.com>
2025-09-01 20:01:56 +0200

    feat: add macro for serializable enums

---
+++ src/binary.rs
@@ -0,0 +1,89 @@
+/// Macro to implement BinRead + BinWrite for repr(u8) enums
+#[macro_export]
+macro_rules! binrw_enum {
+    (
+        $(#[$meta:meta])*
+        $vis:vis enum $name:ident {
+            $($variant:ident = $value:expr),* $(,)?
+        }
+    ) => {
+        $(#[$meta])*
+        #[repr(u8)]
+        $vis enum $name {
+            $($variant = $value),*
+        }
+
+        impl binrw::BinRead for $name {
+            type Args<'a> = ();
+
+            fn read_options<R: binrw::io::Read + binrw::io::Seek>(
+                reader: &mut R,
+                endian: binrw::Endian,
+                _: Self::Args<'_>
+            ) -> binrw::BinResult<Self> {
+                let val = u8::read_options(reader, endian, ())?;
+                match val {
+                    $(x if x == $value => Ok(Self::$variant),)*
+                    other => Err(binrw::Error::AssertFail {
+                        pos: reader.stream_position()?,
+                        message: format!("Invalid value {} for enum {}", other, stringify!($name)),
+                    }),
+                }
+            }
+        }
+
+        impl binrw::BinWrite for $name {
+            type Args<'a> = ();
+
+            fn write_options<W: binrw::io::Write + binrw::io::Seek>(
+                &self,
+                writer: &mut W,
+                endian: binrw::Endian,
+                _: Self::Args<'_>
+            ) -> binrw::BinResult<()> {
+                let val: u8 = match self {
+                    $(Self::$variant => $value),*
+                };
+                val.write_options(writer, endian, ())
+            }
+        }
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    use binrw::{BinRead, BinWrite};
+    use std::io::Cursor;
+
+    binrw_enum! {
+        #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+        pub enum TestSet {
+            Value1 = 1,
+            Value2 = 2,
+            Value3 = 3,
+        }
+    }
+
+    #[test]
+    fn test_enum_read_valid() {
+        let data = [2u8]; // binary data for Value2
+        let mut cursor = Cursor::new(&data);
+        let value = TestSet::read_be(&mut cursor).unwrap();
+        assert_eq!(value, TestSet::Value2);
+    }
+
+    #[test]
+    fn test_enum_read_invalid() {
+        let data = [99u8]; // not mapped to any variant
+        let mut cursor = Cursor::new(&data);
+        let result = TestSet::read_be(&mut cursor);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_enum_write() {
+        let mut buffer = Cursor::new(Vec::new());
+        TestSet::Value3.write_be(&mut buffer).unwrap();
+        assert_eq!(buffer.into_inner(), vec![3u8]);
+    }
+}
